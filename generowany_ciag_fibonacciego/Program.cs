5dc5fa47f2578299986254684decedcdfceb4769
dorian <dorian@DESKTOP-B4SO0PQ>
2022-11-28 21:42:56 +0100

    Dodaj pliki projektów.

---
+++ generowany_ciag_fibonacciego/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace generowany_ciag_fibonacciego
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            int dlugosc;
+            Console.WriteLine("Podaj jakiej długości ma być twój ciąg Fibonacciego: ");
+            dlugosc = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Twój ciąg będzie składał z " + dlugosc + " elementów");
+             Console.WriteLine("Podaj pierwszą liczbę ciągu: ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę ciągu: ");
+            b = double.Parse(Console.ReadLine());
+            for(int i = 0; i < dlugosc; i++)
+            {
+                double c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+
+            }
+        }
+    }
+}
